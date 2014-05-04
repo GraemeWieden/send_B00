1cd3455da4f4acaf0d2d92699db753e7e7137e20
Graeme Wieden <graeme.wieden@gmail.com>
2014-05-04 19:46:59 +1000

    More work supporting new content types
    Cleaned up test block for sending test values

--- send_B00.ino
+++ send_B00.ino
@@ -2,10 +2,10 @@
 // 
 // by default, the descriptor is 0 indicating the following packet payload:
 // 12 bits: B00 - the last byte is the content descriptor
-// 4 bits: 2 bit house code - 2 bit channel code
+// 5 bits: 2 bit house code - 3 bit channel code
 // 32 bits: content - see below
 // 1 bit: even parity bit
-// total: 49 bits
+// total: 50 bits
 
 // Content descriptor currently defines these content types:
 // B00: 
@@ -26,12 +26,14 @@
 // 8 bits: byte value 3
 // 8 bits: byte value 4
 
-#define DEBUG
+// define debug to display output to the console rather than transmitting
+// #define DEBUG
 
 // note that arduino has a max of 16383 for the delayMicroseconds function
-int TriggerPulse = 15000; // trigger time in microseconds
-int LongPulse = 1000; // time in microseconds for a long pulse
-int ShortPulse = LongPulse / 3; // time in microseconds for a short pulse
+int TriggerPulse = 10000; // trigger time in microseconds
+int ShortPulse = TriggerPulse / 31; // time in microseconds for a short pulse
+int LongPulse = ShortPulse * 3; // time in microseconds for a long pulse
+
 boolean parity; // parity bit is tracked as bits are sent
 
 int txPort = 9; // digital pin for transmitter
@@ -42,46 +44,49 @@ byte channel = 1;  // default channel code
 
 void setup()
 {
+#ifdef DEBUG
   Serial.begin(9600);
+#endif
+
   pinMode(ledPort, OUTPUT);   
 }
 
 void loop()
 {
-  double testDouble = 123456.789;
+  double testDouble = 12345.67;
   sendB00(house, channel, testDouble);
   blinkAndPause();
-  
-  testDouble = -9876.54321;
+
+  testDouble = -9876.543;
   sendB00(house, channel, testDouble);
   blinkAndPause();
-  
+
   long testLong = 123456789;
   sendB01(house, channel, testLong);
   blinkAndPause();
-  
+
   testLong = -987654321;
   sendB01(house, channel, testLong);
   blinkAndPause();
-  
+
   unsigned long testUnsignedLong = 1234567890;
   sendB02(house, channel, testUnsignedLong);
   blinkAndPause();
-  
-  int testA = -20;
-  int testB = 20;
+
+  int testA = -32123;
+  int testB = 32123;
   sendB03(house, channel, testA, testB);
   blinkAndPause();
 
   unsigned int testC = 0;
-  unsigned int testD = 65530;  
-  sendB04(house, channel, testC, testD++);
+  unsigned int testD = 65432;  
+  sendB04(house, channel, testC, testD);
   blinkAndPause();
-  
+
   sendB05(house, channel, B10101010, B11001100, B11110000, B11111111);
   blinkAndPause();  
-  
-  delay(10000);
+
+  delay(5000);
 }
 
 void blinkAndPause()
@@ -92,14 +97,32 @@ void blinkAndPause()
   digitalWrite(ledPort, LOW);
   delay(1000);
 }
+
+//----------------------------------------------------------------
+// double values can range from -3.4028235E+38 to 3.4028235E+38
+// but only have a maximum of 6 to 7 decimal digits of precision
+//----------------------------------------------------------------
+
 void sendB00(byte house, byte channel, double value)
 {
 #ifdef DEBUG
   Serial.println("B00 : double");
 #endif
-
-  sendB00Packet(0, house, channel, value);
+  // we have a special case here to convert the double to an unsigned it without casting.
+  // to do this, we create a union of the double and an unsigned long. this means they share
+  // the same memory and so we can treat them as the same bit pattern
+  union
+  {
+    double dblValue;
+    unsigned long ulValue;
+  };
+  dblValue = value;
+  sendB00Packet(0, house, channel, ulValue);
 }
+
+//----------------------------------------------------------------
+// long values can range from -2,147,483,648 to 2,147,483,647
+//----------------------------------------------------------------
 
 void sendB01(byte house, byte channel, long value)
 {
@@ -110,6 +133,10 @@ void sendB01(byte house, byte channel, long value)
   sendB00Packet(1, house, channel, value);
 }
 
+//----------------------------------------------------------------
+// unsigned long values can range from 0 to 4,294,967,295
+//----------------------------------------------------------------
+
 void sendB02(byte house, byte channel, unsigned long value)
 {
 #ifdef DEBUG
@@ -118,6 +145,10 @@ void sendB02(byte house, byte channel, unsigned long value)
 
   sendB00Packet(2, house, channel, value);
 }
+
+//----------------------------------------------------------------
+// int values can range from -32,768 to 32,767
+//----------------------------------------------------------------
 
 void sendB03(byte house, byte channel, int valueA, int valueB)
 {
@@ -130,6 +161,10 @@ void sendB03(byte house, byte channel, int valueA, int valueB)
   sendB00Packet(3, house, channel, value);
 }
 
+//----------------------------------------------------------------
+// unsigned int values can range from 0 to 65,535
+//----------------------------------------------------------------
+
 void sendB04(byte house, byte channel, unsigned int valueA, unsigned int valueB)
 {
 #ifdef DEBUG
@@ -141,23 +176,30 @@ void sendB04(byte house, byte channel, unsigned int valueA, unsigned int valueB)
   sendB00Packet(4, house, channel, value);
 }
 
+//----------------------------------------------------------------
+// byte values can range from 0 to 255
+//----------------------------------------------------------------
+
 void sendB05(byte house, byte channel, byte b1, byte b2, byte b3, byte b4)
 {
 #ifdef DEBUG
   Serial.println("B05 : byte byte byte byte");
 #endif
 
-  unsigned long value = (b1 << 24) | (b2 << 16) | (b3 << 8) | b4;
+  unsigned long value = b1;
+  value = value << 8 | b2;
+  value = value << 8 | b3;
+  value = value << 8 | b4;
   sendB00Packet(5, house, channel, value);
 }
 
 void sendB00Packet(byte content, byte house, byte channel, unsigned long value)
 {
-  int repeats = 3;
-  #ifdef DEBUG
-    repeats = 1;
-  #endif
-  
+  int repeats = 4;
+#ifdef DEBUG
+  repeats = 1;
+#endif
+
   sendB00Trigger();
 
   for(int i = 0; i < repeats; i++)
@@ -165,10 +207,10 @@ void sendB00Packet(byte content, byte house, byte channel, unsigned long value)
     // parity = 0; // don't need to do this as parity bit is reset to zero by writing the parity bit
     sendB00Bits(0xB, 4); // announce!
     sendB00Bits(content, 8); // default 00
-    sendB00Bits(house, 2);
-    sendB00Bits(channel, 2);
-    sendB00Bits(value, 32);
-    sendB00Bits(parity, 1);
+    sendB00Bits(house, 2); // 0-3
+    sendB00Bits(channel, 3); // 0-7
+    sendB00Bits(value, 32); // content dependent
+    sendB00Bits(parity, 1); // even parity
     sendB00Trigger();
   }
 #ifdef DEBUG
@@ -179,7 +221,7 @@ void sendB00Packet(byte content, byte house, byte channel, unsigned long value)
 void sendB00Trigger()
 {
   digitalWrite(txPort, HIGH);
-  delayMicroseconds(TriggerPulse);   
+  delayMicroseconds(ShortPulse);   
   digitalWrite(txPort, LOW);
   delayMicroseconds(TriggerPulse);    
 }
@@ -230,6 +272,9 @@ void debugB00Bit(byte b)
     parity = !parity;
   }
 }
+
+
+
 
 
 
